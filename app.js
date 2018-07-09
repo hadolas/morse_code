889f255791d086719f1d3439e1b208f4a3901b6e
hadolas <hadiyahmughal@gmail.com>
2018-07-09 01:37:19 +0000

    Transate from eng-2-morse and from morse-2-eng on same page; display morse key guide on main page

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 
-var morseTranslation = {
+var engToMorse = {
     "A" : ".-",
     "B" : "-...",
     "C" : "-.-.",
@@ -29,10 +29,10 @@ var morseTranslation = {
     "X" : "-..-",
     "Y" : "-.--",
     "Z" : "--..",
-    " " : "      "
+    " " : " "
 }
 
-var englishTranslation = {
+var morseToEng = {
     ".-"   : "A",
     "-..." : "B",
     "-.-." : "C",
@@ -69,60 +69,63 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"));
 
 
-//REDIRECT ROOT ROUTE
+//ROOT ROUTE - REDIRECT TO /english-to-morse
 app.get("/", function(req, res){
     res.redirect("/english-to-morse");
 });
 
+
 //RENDER english-to-morse
 app.get("/english-to-morse", function(req, res){
-   res.render("english-to-morse");
+    //loop through engToMorseObj and add each key:value to the 'keyGuide' string
+    var keyGuide="";
+    for(var p in engToMorse){
+        if(engToMorse.hasOwnProperty(p)){
+            keyGuide += p + " , " + engToMorse[p] +"\n";
+        }
+    }
+    console.log("This is keyguide: "+keyGuide);
+    res.render("english-to-morse", {keyGuide:keyGuide});
 });
 
-//Translate user input (English) into Morse and render result page.
+//Translate user input into English or Morse and render result page.
 app.post("/english-to-morse", function(req, res){
     var input = req.body.user_input.toUpperCase();
-    var result = ""
-    for(var i=0; i<input.length; i++){
-        for(var key in morseTranslation){
-            //if the character in the input string is equal to the object key...
-            if(input[i]===key){
-                //...append the corresponding object key value (Morse character) to 'result' string
-                result = result+" "+morseTranslation[key];
-            }
-        }
-    }
-    console.log(result);
-    res.render("result", {result:result});
-});
-
-//RENDER morse-to-english
-app.get("/morse-to-english", function(req, res){
-    res.render("morse-to-english");
-});
-
-app.post("/morse-to-english", function(req, res){
-    var input = req.body.user_input.split(" ");
-    var result=""
-    console.log(input);
+    var result="";
+    // if the first character in user input is NOT "." or "-" then convert from English to Morse
+    if(input[0]!=="."||input[0]==="-"){
         for(var i=0; i<input.length; i++){
-        for(var key in englishTranslation){
-            //if the character in the input string is equal to the object key...
-            if(input[i]===key){
-                //...append the corresponding object key value (Morse character) to 'result' string
-                result = result+englishTranslation[key];
+            for(var key in engToMorse){
+                //if the character in the input string is equal to the object key...
+                if(input[i]===key){
+                    //...append the corresponding object key value (Morse character) to 'result' string
+                    result = result+" "+engToMorse[key];
+                }
+            }
+        }
+        console.log(result);
+    //ELSE (if the first character in user input IS a "." or "-") THEN convert from Morse to English
+    } else {
+        //Create a list where user input is split at each " "
+        input=input.split(" ");
+        for(var i=0; i<input.length; i++){
+            for(var key in morseToEng){
+                if(input[i]===key){
+                    result = result+morseToEng[key];
+                }
             }
         }
     }
-    console.log(result);
     res.render("result", {result:result});
 });
+
 
 //RENDER morse key guide
-
 app.get("/morse-key-guide", function(req, res){
    res.send("Morse Key Guide page"); 
 });
+
+
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Morse Convertor server initialised.");
